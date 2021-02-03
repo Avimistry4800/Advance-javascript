ea4422a72b61c088c0fcb389196084b6a8727049
Meghkonna Rumi <avimistry4800@gmail.com>
2021-02-03 15:22:14 +0600

    array-split>slice-splice-join

---
+++ array-split.js
@@ -0,0 +1,9 @@
+const nums = [1,2,3,4,5,6,7];
+console.log(nums);
+// const part = nums.slice(2,5);
+const removed = nums.splice(2,1,23 , 54, 55);
+// console.log(nums);
+// console.log(removed);
+// const togather = nums.join("");
+const togather = nums.join("ami");
+console.log(togather);
